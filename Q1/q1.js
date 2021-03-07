261572bb4850af62232e57e7939b621a2055cc47
Tan Liang Yit <liangyit@gmail.com>
2021-03-07 21:02:10 +0800

    commit

--- Q1/q1.js
+++ Q1/q1.js
@@ -1,6 +1,7 @@
-var max = 1*prompt ("Please enter an integer.");
+var max = 1*prompt ("Please enter a number:");
 var sum = 0;
 for (var i=0; i<=max; i++)
  sum = sum + i;
-console.log("The average of 0 to "+max+" is "+(sum/(max+1))); 
+ average = sum/(max+1);
+console.log("The average of 0 to "+max+" is "+ average); 
 
